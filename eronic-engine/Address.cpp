33f356ba8cc8063ab245b56bf94a744a83773018
Ephra <es.90@gmx.de>
2018-04-11 02:03:27 +0200

    fixed ip init bug

--- eronic-engine/Address.cpp
+++ eronic-engine/Address.cpp
@@ -2,6 +2,7 @@
 
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <iostream>
 
 Address::Address() :
 	_ip("127.0.0.1"),
@@ -20,18 +21,40 @@ Address::Address(std::string &ip, int port) :
 {
 	_socket_address.sin_family = AF_INET;
 	_socket_address.sin_port = htons(_port);
-	if (_ip == "ANY") {
+	if (_ip == "ADDR_ANY") {
 		//InetPton(AF_INET, INADDR_ANY, &_socket_address.sin_addr.S_un.S_addr);
-		//inet_pton(AF_INET, ADDR_ANY, &(_socket_address.sin_addr));
-		// _socket_address.sin_addr.S_un.S_addr =
-		//inet_pton(AF_INET, INADDR_ANY, &(_socket_address.sin_addr.S_un.S_addr));
 		//inet_pton(AF_INET, INADDR_ANY, &(_socket_address.sin_addr));
 		_socket_address.sin_addr.S_un.S_addr = INADDR_ANY;
+		char address_ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(_socket_address.sin_addr), address_ip, INET_ADDRSTRLEN);
+		_ip = address_ip;
 	}
 	else {
 		inet_pton(AF_INET, _ip.c_str(), &(_socket_address.sin_addr));
 		//_socket_address.sin_addr.S_un.S_addr = inet_pton(AF_INET, _ip.c_str(), &_socket_address.sin_addr.S_un.S_addr);
 	}
+}
+
+
+Address::~Address()
+{
+}
+
+sockaddr_in const* Address::get_socket_address() const
+{
+	return &_socket_address;
+}
+
+int Address::get_port() const
+{
+	return _port;
+}
+
+std::string const& Address::get_ip() const
+{
+	char address_ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(_socket_address.sin_addr), address_ip, INET_ADDRSTRLEN);
+	return _ip;
 }
 
 //Address::Address(sockaddr * addr) :
@@ -51,23 +74,21 @@ Address::Address(std::string &ip, int port) :
 //	_socket_address.sin_port = htons(_port);
 //	InetPton(AF_INET, _ip.c_str(), &_socket_address.sin_addr.S_un.S_addr);*/
 //}
+//
 
+////inet_ntoa(struct in_addr in);
+//struct sockaddr_in {
+//u_short     sin_family;
+//u_short     sin_port;
+//struct      in_addr sin_addr;
+//char        sin_zero[8];
+//};
+////
+//char adress_ip[INET_ADDRSTRLEN];
+//
+//// now get it back and print it
+//inet_ntop(AF_INET, &(_socket_address.sin_addr), adress_ip, INET_ADDRSTRLEN);
+//std::cout << "ADRESS IP" << adress_ip << std::endl;
 
-Address::~Address()
-{
-}
-
-sockaddr_in const* Address::get_socket_address() const
-{
-	return &_socket_address;
-}
-
-int Address::get_port() const
-{
-	return _port;
-}
-
-std::string const& Address::get_ip() const
-{
-	return _ip;
-}
+// printf("%s\n", str); // prints "192.0.2.33"
+//InetNtop()
