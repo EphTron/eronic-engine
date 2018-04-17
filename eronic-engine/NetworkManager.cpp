6de2b663131dba0f8ee3c66e35984d57617950ca
ephtron <es.90@gmx.de>
2018-04-17 12:16:00 +0100

    added Networkmanager class

--- eronic-engine/NetworkManager.cpp
+++ eronic-engine/NetworkManager.cpp
@@ -13,13 +13,57 @@ namespace eronic {
 
 	std::vector<Network*> NetworkManager::find_networks()
 	{
-		_peer->find_networks(7500, false);
-		return std::vector<Network*>();
+		std::vector<Network*> res = std::vector<Network*>();
+		res = _peer->find_networks(7500, false);
+		networks = res;
+		return res;
 	}
 
-	void NetworkManager::run_network_loop()
+	void NetworkManager::open_network(int net_id, int net_port)
+	{
+		_peer->open_network(net_id, net_port);
+		_connected = true;
+	}
+
+	void NetworkManager::join_network(int net_id, int net_port)
+	{
+		_peer->join_network(net_id, net_port);
+		_connected = true;
+	}
+
+	void NetworkManager::leave_network()
+	{
+	}
+
+	void NetworkManager::retry_network()
+	{
+	}
+
+	void NetworkManager::start()
 	{
 		_peer->run_peer_network();
 	}
+
+	void NetworkManager::stop()
+	{
+	}
+
+	void NetworkManager::close()
+	{
+	}
+
+	std::vector<DataPackage*> NetworkManager::get_network_messages()
+	{
+		return std::vector<DataPackage*>();
+	}
+
+	void NetworkManager::broadcast_message()
+	{
+	}
+
+	void NetworkManager::send_private_message()
+	{
+	}
+
 
 } // namespace eronic
--- eronic-engine/NetworkManager.h
+++ eronic-engine/NetworkManager.h
@@ -13,22 +13,26 @@ namespace eronic {
 		~NetworkManager();
 
 		std::vector<Network*> find_networks();
-		void open_network();
-		void join_network();
+		void open_network(int net_id, int net_port);
+		void join_network(int net_id, int net_port);
+		void leave_network();
+		void retry_network();
 
-		void run_network_loop();
+		void start();
+		void stop();
+		void close();
 
-		std::vector<DataPackage*> get_network_data();
-		void send_data_to_network();
-
-
-
-
-
+		std::vector<DataPackage*> get_network_messages();
+		void broadcast_message();
+		void send_private_message();
 
 
 	private:
+		bool _connected;
+		bool _fully_connected;
 		PeerNode * _peer;
+		std::vector<Network*> networks;
+		std::map<int, PeerPartner*> participants;
 		//std::vector<DataPackage*> _data_buffer;
 
 		void collect_network_data();
